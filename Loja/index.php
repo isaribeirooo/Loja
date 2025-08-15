1c41f89b48196531ebfa0290c5e2524fd83e04dd
isaribeirooo <161508466+isaribeirooo@users.noreply.github.com>
2025-08-15 17:25:18 -0300

    Add files via upload

---
+++ Loja/index.php
@@ -0,0 +1,18 @@
+<?php
+   include 'cabecalho.php';
+?>
+<body>
+    <div class="container">
+        <h1>Bem vindo ao 1° Sistema com CRUD</h1>
+        <h2>Isadora Ribeiro e Ana Portela</h2>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+        <a href= "#" type="a" class="btn btn-danger">Cadastrar</a>
+        <a href= "#" type="a" class="btn btn-warning">Listar</a>
+        </div>
+    </div>
+     
+    <?php
+    include 'rodape.php';
+    ?>
+</body>
+</html>
---
+++ Loja/listar.php
@@ -0,0 +1,32 @@
+<?php
+include 'cabecalho.php';
+?>
+<body>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">NOME</th>
+                    <th scope="col">PREÇO</th>
+                    <th scope="col">QUANTIDADE</th>
+                    <th scope="col">OPÇÔES</th>
+            </tr>
+         </thead>
+        <tbody>
+            <tr>  
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>
+                    <div class="btn-group" role="group" arial-label="Basic mixed styles example">
+                        <a href="#" type="button" class="btn btn-danger">Atualizar</a>
+                        <a href="#" type="button" class="btn btn-warning">Apagar</a>
+            <tr>
+          </tbody>
+        </table>
+       </div>
+     </body>
+   </html>
