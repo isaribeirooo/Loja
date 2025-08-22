0240028eeed5b1289c618df9c039228e12964666
isaribeirooo <161508466+isaribeirooo@users.noreply.github.com>
2025-08-22 16:56:35 -0300

    Add files via upload

---
+++ loja/index.php
@@ -0,0 +1,17 @@
+<?php
+   include 'cabecalho.php';
+?>
+<body>
+    <div class="container">
+        <h1>Bem vindo ao 1° Sistema com CRUD</h1>
+        <h2>Isadora Ribeiro e Ana Portela</h2>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+        <a href= "form_cadastrar.php" type="a" class="btn btn-danger">Cadastrar</a>
+        <a href= "listar.php" type="a" class="btn btn-warning">Listar</a>
+        </div>
+    </div>
+     
+    <?php
+    ?>
+</body>
+</html>
