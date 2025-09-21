2fa81ee6b8a99c73461c3015b607e856ae463b61
isaribeirooo <161508466+isaribeirooo@users.noreply.github.com>
2025-09-20 23:42:19 -0300

    alterações

--- loja/index.php
+++ loja/index.php
@@ -8,7 +8,7 @@
 $stmt = $pdo->query("SELECT * FROM produtos");
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo "<div class='produto'>";
-        echo "<img src='{$row['imagem']}' alt='{$row['nome']}'>";
+
             echo "<h3>{$row['nome']}</h3>";
                 echo "<p class='preco'>R$ " . number_format($row['preco'], 2, ',', '.') . "</p>";
                     
